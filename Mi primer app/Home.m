a337ed0ee21a0427a680e6a84fd57371c66447d9
Mac Mini <macmini@Macs-Mac-mini.local>
2017-09-17 20:45:14 -0500

    resto de archivos mi primer app

---
+++ Mi primer app/Home.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Mi primer app
+//
+//  Created by Mac Mini on 9/17/17.
+//  Copyright © 2017 lybby26. All rights reserved.
+//
+
+#import "Home.h"
+
+@interface Home ()
+
+@end
+
+@implementation Home
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
