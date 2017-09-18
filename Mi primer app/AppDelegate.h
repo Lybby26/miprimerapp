b519e6568f055489230150097c775629df2e88ac
Mac Mini <macmini@Macs-Mac-mini.local>
2017-09-17 19:30:10 -0500

    Initial Commit

---
+++ Mi primer app/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Mi primer app
+//
+//  Created by Mac Mini on 9/17/17.
+//  Copyright © 2017 lybby26. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Mi primer app/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mi primer app
+//
+//  Created by Mac Mini on 9/17/17.
+//  Copyright © 2017 lybby26. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
